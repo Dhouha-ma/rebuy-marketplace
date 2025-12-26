c3af429a023b5a496beee0f66fce04d055ec397e
halseh92 <halim.allemagne@gmail.com>
2025-12-26 14:34:35 +0100

    add vot button component

--- src/app/components/card/card.ts
+++ src/app/components/card/card.ts
@@ -3,13 +3,47 @@ import { CommonModule } from '@angular/common';
 
 import { Button } from '../shared/button/button';
 import { Offer } from '../../types/offer-type';
+import { VoteButton } from '../shared/vote-button/vote-button';
 
 @Component({
   selector: 'app-card',
-  imports: [Button, CommonModule],
+  imports: [Button, CommonModule, VoteButton],
   templateUrl: './card.html',
   styleUrl: './card.scss',
 })
 export class Card {
   @Input() offer!: Offer;
+
+  public liked = false;
+  public disliked = false;
+
+  public onLike() {
+    if (this.disliked) {
+      this.disliked = false;
+      this.offer.dislikes--;
+    }
+
+    if (this.liked) {
+      this.liked = false;
+      this.offer.likes--;
+    } else {
+      this.liked = true;
+      this.offer.likes++;
+    }
+  }
+
+  public onDislike() {
+    if (this.liked) {
+      this.liked = false;
+      this.offer.likes--;
+    }
+
+    if (this.disliked) {
+      this.disliked = false;
+      this.offer.dislikes--;
+    } else {
+      this.disliked = true;
+      this.offer.dislikes++;
+    }
+  }
 }
---
+++ src/app/components/shared/vote-button/vote-button.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-vote-button',
+  imports: [],
+  templateUrl: './vote-button.html',
+  styleUrl: './vote-button.scss',
+})
+export class VoteButton {
+  @Input() icon: string = '';
+  @Input() count: number = 0;
+  @Input() active: boolean = false;
+  @Input() type: 'like' | 'dislike' = 'like';
+
+  @Output() vote = new EventEmitter<void>();
+
+  public onClick() {
+    this.vote.emit();
+  }
+}
