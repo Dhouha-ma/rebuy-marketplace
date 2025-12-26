6dff57d3d4d94c5a64c3b2cc9f725975378373c8
halseh92 <halim.allemagne@gmail.com>
2025-12-26 10:04:44 +0100

    prepare routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { OffersList } from './components/offers-list/offers-list';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'offers', pathMatch: 'full' },
+  { path: 'offers', component: OffersList },
+  { path: 'my-orders', component: OffersList },
+  { path: 'contact', component: OffersList },
+];
