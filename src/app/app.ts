a34abbd6d6eee456220a1474639015f54d662500
halseh92 <halim.allemagne@gmail.com>
2025-12-26 12:10:55 +0100

    Add navbar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('rebuy-marketplace');
-
-  constructor(private router: Router) {}
-
-  public navigateTo(path: string) {
-    this.router.navigateByUrl(path);
-  }
 }
---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {
+  public menuOpen = false;
+  public currentPage = 'offers';
+
+  constructor(private router: Router) {}
+
+  public toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  public navigateTo(page: string) {
+    this.currentPage = page;
+    this.router.navigate([page]);
+  }
+}
